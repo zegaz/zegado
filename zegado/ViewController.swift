a3719750383b17853d6c3682e17853ff0b849d3e
Geza Varga <zega@vargabrothers.hu>
2018-10-15 09:48:15 +0200

    Initial Commit

---
+++ zegado/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zegado
+//
+//  Created by Géza Varga on 2018. 10. 15..
+//  Copyright © 2018. Géza Varga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
